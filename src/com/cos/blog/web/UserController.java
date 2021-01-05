89f9f4f206abeb4d5da222b9cd28fc88341f88cc
sksdl9749 <sksdl9749@gmail.com>
2021-01-05 17:20:01 +0900

    8. 로그인 구현

--- src/com/cos/blog/web/UserController.java
+++ src/com/cos/blog/web/UserController.java
@@ -9,7 +9,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.cos.blog.domain.user.User;
 import com.cos.blog.domain.user.dto.JoinReqDto;
 import com.cos.blog.domain.user.dto.LoginReqDto;
 import com.cos.blog.service.UserService;
@@ -48,7 +50,15 @@ public class UserController extends HttpServlet {
 			LoginReqDto dto = new LoginReqDto();
 			dto.setUsername(username);
 			dto.setPassword(password);
-			userService.로그인(dto);
+			
+			User userEntity = userService.로그인(dto);
+			if(userEntity != null) {
+				HttpSession session = request.getSession();
+				session.setAttribute("principal", userEntity); // 인증주체
+				response.sendRedirect("index.jsp");
+			} else {
+				Script.back(response, "로그인 실패");
+			}
 		} else if(cmd.equals("joinForm")) {
 			response.sendRedirect("user/joinForm.jsp");
 		} else if(cmd.equals("join")) {
@@ -81,6 +91,10 @@ public class UserController extends HttpServlet {
 				out.print("fail");
 			}
 			out.flush();
+		} else if(cmd.equals("logout")) {
+			HttpSession session = request.getSession();
+			session.invalidate();
+			response.sendRedirect("index.jsp");
 		}
 	}
 
